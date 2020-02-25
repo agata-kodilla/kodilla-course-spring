8e3d88fb02c6e4eacd7e9e4d0dd1bdb67f19a3bc
abialkowska <abialkowska247@gmail.com>
2020-02-25 19:50:07 +0100

    add spring intro

---
+++ src/main/java/kodilla/spring/intro/shape/Shape.java
@@ -0,0 +1,5 @@
+package kodilla.spring.intro.shape;
+
+public interface Shape {
+    String draw();
+}
---
+++ src/main/java/kodilla/spring/intro/shape/Triangle.java
@@ -0,0 +1,9 @@
+package kodilla.spring.intro.shape;
+
+public class Triangle implements Shape {
+    @Override
+    public String draw() {
+        System.out.println("This is a triangle");
+        return "This is a triangle";
+    }
+}
