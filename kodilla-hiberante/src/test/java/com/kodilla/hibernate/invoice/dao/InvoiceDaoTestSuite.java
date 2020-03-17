1f058da5d6912e7cd77286b9aebe99f18f4dbcb0
abialkowska <abialkowska247@gmail.com>
2020-03-17 20:35:33 +0100

    exercise 3 hiberante

---
+++ kodilla-hiberante/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,49 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import com.kodilla.hibernate.task.Task;
+import com.kodilla.hibernate.task.TaskFinancialDetails;
+import com.kodilla.hibernate.task.dao.TaskDao;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Product product1 = new Product("product1");
+        Item item1 = new Item(product1, new BigDecimal(150), 3, new BigDecimal(450));
+        Item item2 = new Item(product1, new BigDecimal(10), 2, new BigDecimal(20));
+        List<Item> items =new ArrayList<>();
+        items.add(item1);
+        items.add(item2);
+        Invoice invoice = new Invoice();
+        invoice.setItems(items);
+
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //Then
+        Assert.assertNotEquals(0, id);
+
+        //CleanUp
+        //taskDao.deleteById(id);
+    }
+}
